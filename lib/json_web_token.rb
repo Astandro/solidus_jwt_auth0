4027ead5ffacf74645639a3e86b7311425f407ee
astandro <astandro.tc@gmail.com>
2021-10-17 16:18:58 +0700

    add method to verify id token

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -15,6 +15,18 @@ class JsonWebToken
     end
   end
 
+  def self.verify_id(token)
+    JWT.decode(token, nil,
+               true, # Verify the signature of this token
+               algorithms: 'RS256',
+               iss: 'https://greedy.au.auth0.com/',
+               verify_iss: true,
+               aud: 'sMtJgmTT4V09OyX74fEzfV0c9fLjMP1L',
+               verify_aud: true) do |header|
+      jwks_hash[header['kid']]
+    end
+  end
+
   def self.jwks_hash
     jwks_raw = Net::HTTP.get URI("https://greedy.au.auth0.com/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
